e20e50a19b0d287bc60df4e3d796da4c536e15e6
Vera Scheunert <vera.scheunert@aol.com>
2020-05-14 08:03:11 +0200

    Add implementation for last EventCard test suite

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <h2>{{ event.title }}</h2>
+    <h3>{{ newDateFormat }}</h3>
     <p>{{ event.location }}</p>
     <p>{{ event.details }}</p>
   </div>
@@ -9,6 +10,11 @@
 <script>
 export default {
   props: ["event"],
+  computed: {
+    newDateFormat() {
+      return new Date(this.event.date).toDateString();
+    },
+  },
 };
 </script>
 
